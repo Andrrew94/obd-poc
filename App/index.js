bb5a00a1fd7c54d801cc810c4b75438bb6660369
Andrei <andrei.sandica.94@gmail.com>
2024-05-31 21:04:20 +0300

    TODO

--- App/index.js
+++ App/index.js
@@ -46,4 +46,16 @@ const main = async () => {
   }
 };
 
+
+// TODO: identify how to dynamically set BAUD RATE for faster connection
+// TODO: do we run the OBD2 INITIALIZATION COMMANDS? 
+// (After establishing the connection, you may need to send initialization commands. Common initialization commands for OBD-II adapters include setting the protocol and ensuring the adapter is in a proper state to communicate.)
+// const commands = [
+//   { command: 'ATZ', response: 'OK' }, // Reset
+//   { command: 'ATE0', response: 'OK' }, // Echo off
+//   { command: 'ATSP0', response: 'OK' }, // Set protocol to automatic
+//   { command: '0100', response: '41 00' } // Check device status
+// ];
+
+
 main();
