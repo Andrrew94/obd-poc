76ee49f3e54be1154439367d3fb2a89202d071ba
Andrei <andrei.sandica.94@gmail.com>
2024-06-02 12:34:52 +0300

    prompt to save result to json

--- App/index.js
+++ App/index.js
@@ -1,5 +1,33 @@
 const { disconnectOBD, getSupportedPIDs, getSupportedPidValues } = require('./bluetoothManager');
 const { discoverDevices, identifyOBDDevice } = require('./discoverDevices');
+const fs = require('fs');
+const readline = require('readline');
+
+// Function to prompt user and save JSON
+const promptSaveToFile = (data) => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.question('Do you want to save the data to a JSON file? (yes/no): ', (answer) => {
+    if (answer.toLowerCase() === 'yes') {
+      rl.question('Enter the file name (no extension): ', (fileName) => {
+        fs.writeFile(`${fileName}.json`, JSON.stringify(data, null, 2), (err) => {
+          if (err) {
+            console.error('Error saving file:', err);
+          } else {
+            console.log('File saved successfully!');
+          }
+          rl.close();
+        });
+      });
+    } else {
+      console.log('Data not saved.');
+      rl.close();
+    }
+  });
+};
 
 const main = async () => {
   try {
@@ -23,6 +51,8 @@ const main = async () => {
 
       const allPIDValues = await getSupportedPidValues(serial);
       console.log(JSON.stringify(allPIDValues, null, 2));
+
+      promptSaveToFile(allPIDValues);
 
       // Check if PID 0D is supported
       // if (supportedPIDs.includes('0D')) {
